b8eaa7c7cb78db9a0dd10276259316be2c90a4f7
BrownKnight <amanrk@hotmail.co.uk>
2017-10-04 19:22:49 +0100

    php script for getting questions

---
+++ scripts/getQuestions.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: adhoot
+ * Date: 04/10/2017
+ * Time: 19:14
+ *
+ * Designed to retun all questions stored in the db in a josn format
+ */
+
+//Create Connection to DB
+
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+// Create connection
+$conn = mysqli_connect($hostname, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+//Create SQL to get a list of all questions
+$sql = "SELECT * FROM questions";
+
+// Query db with statement
+$res = mysqli_query($conn, $sql);
+
+echo json_encode(mysqli_fetch_assoc($res));
