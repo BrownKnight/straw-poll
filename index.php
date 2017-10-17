f96aecf45146ce60b5f6127d0b6953baa88dac8b
BrownKnight <amanrk@hotmail.co.uk>
2017-10-17 15:22:40 +0100

    breakpoints

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 
-<html>
+<html lang="en">
 <?php
     
     session_start();
@@ -30,10 +30,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title> Straw Poll </title>
 
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
     <script src="js/jquery-3.2.1.min.js"></script>
     <script src="js/script.js"></script>
-    
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+
 </head>
 <body>
     <div class="container">
@@ -51,6 +57,12 @@
             <input type="button" onclick="getQuestionList()">
         </div>
     </div>
+<button type="button" class="btn btn-danger">Show alert</button>
+<button>nkjdbo</button>
+
+<div class="alert alert-danger alert-dismissible fade show" role="alert"> This is an alert!
+    <button role="button" type="button" aria-label="Close" data-dismiss="alert"></button>
+</div>
 
 </body>
 </html>
